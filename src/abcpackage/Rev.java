e672406c904ba0a830de7a7b5e48f68fa5119f2f
Harish <Harish@Harish-PC>
2019-04-03 12:49:22 +0530

    read the line

---
+++ src/abcpackage/Rev.java
@@ -0,0 +1,13 @@
+package abcpackage;
+
+public class Rev
+{
+public static void main(String[] args) 
+{
+String s="Harish";
+for(int i=s.length()-1;i>=0;i--)
+{
+	System.out.print(s.charAt(i));
+}
+}
+}
